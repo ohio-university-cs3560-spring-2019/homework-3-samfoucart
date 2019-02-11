9e902a7c23c4776665c5f0b78166dace02f14be3
Sam Foucart <sf241616@ohio.edu>
2019-02-11 12:10:26 -0500

    Fixed hw_broken.cc and added a header file to call it.

--- hw_broken.cc
+++ hw_broken.cc
@@ -1,36 +1,25 @@
 #include <iostream>
-#include <vector>
 #include <cmath>
+#include "deviation.h"
 
-int deviation( int* a, int n);
-
-int main()
+double samfoucart::deviation(int * a, int n )
 {
-	std::vector< int > v;
-	int _10 = 10;
-	while( _10 >= 0 )
-	{
-		v.push_back(_10);
-		std::cout << deviation(v) << std::endl;
-		_10 - 1;
-	}
-}
-
-int deviation( int* a, int n )
-{
-	int sum;
-	for(size_t i = 0; i <= v.size(); i++)
-	{
-		sum += v[i];
-	} 
-	double mean = sum /= v.size();
-	double stddev = 0;
-	for(size_t i = 0; i <= v.size() -1; i++)
-	{
-		double stddev = stddev + (v[i] - mean) * (v[i] - mean); 
-	}
-	stddev /= v.size();
-	if( stddev = 0)
-		std::cout << "Sigma is zero." << std::endl;
-	return sqrt(stddev);
+  int sum = 0;
+  for(size_t i = 0; i < n; i++)
+    {
+      sum += a[i];
+    } 
+  double mean = sum / n;
+  double stddev = 0;
+  for(size_t i = 0; i < n; i++)
+    {
+      stddev = stddev + ((a[i] - mean) * (a[i] - mean)); 
+    }
+  stddev /= n;
+  if( stddev == 0)
+    {
+      std::cout << "Sigma is zero." << std::endl;
+      return 0;
+    }
+  return sqrt(stddev);
 }
---
+++ src/deviation.h
@@ -0,0 +1,9 @@
+#ifndef DEVIATION_H
+#define DEVIATION_H
+
+namespace samfoucart
+{
+  double deviation(int * a, int n);
+}
+
+#endif
