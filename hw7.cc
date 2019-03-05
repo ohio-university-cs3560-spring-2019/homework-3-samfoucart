30368c830cdc6789ca5ee098e85e042689eeb9c3
Sam Foucart <sf241616@ohio.edu>
2019-03-04 23:14:35 -0500

    Added comments to hw7

--- hw7.cc
+++ hw7.cc
@@ -1,14 +1,32 @@
+/***********************************
+ * @file hw7.cc
+ * @author Sam Foucart
+ * @date 3/4/19
+ * 
+ * This program calculates the standard deviation
+ * of a variac set of numbers.
+ *
+ ***********************************/
+
 #include "hw7.h"
 #include <cmath>
 #include <cstdarg>
 
 double stddev(int n, ...)
 {
+  // Total is the sum of the set
   double total = 0;
+  // Average is the average of the set
   double average = 0;
+  // Variance is the variance of the set
   double variance = 0;
+  // Value is each individual value of the set
   int value;
 
+  /////////////////////////////////
+  // Getting the Total of the set
+  /////////////////////////////////
+  
   va_list args;
   va_start(args, n);
   for (size_t i = 0; i < n; ++i)
@@ -18,6 +36,10 @@ double stddev(int n, ...)
   va_end(args);
 
   average = total / n;
+
+  /////////////////////////////////
+  // Calculating the variance
+  /////////////////////////////////
 
   va_start(args, n);
   for (size_t i = 0; i < n; ++i)
